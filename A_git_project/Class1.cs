0a1d8f72d7314d8a8edf344a35a2c787cad8b63f
Ajay Puradadoddi <ajay.puradadoddi@acumant.com>
2025-02-14 10:40:10 +0530

    Add project files.

---
+++ A_git_project/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_git_project
+{
+    class Class1
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method1");
+        }   
+        public static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            c.Method1();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+}
